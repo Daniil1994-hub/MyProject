b20992dfb5e64340e1f469b9a175d8870ddd27d7
Daniil1994-hub <tarandaniil22@gmail.com>
2025-08-29 16:42:43 +0300

    Выполнение задания

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Test", "Testov", (byte) 25);
+        userServiceImpl.saveUser("Ivan", "ivanov", (byte) 30);
+        userServiceImpl.saveUser("Petr", "Petrov", (byte) 35);
+        userServiceImpl.saveUser("Danil", "Danilov", (byte) 31);
+        userServiceImpl.getAllUsers();
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
+
+    }
+
+}
