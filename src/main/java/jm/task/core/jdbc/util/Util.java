3e71c047a7b6063598c49631b99f99844ea36dbb
Daniil1994-hub <tarandaniil22@gmail.com>
2025-09-09 22:53:01 +0300

    Доработанный вариант задачи 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,9 +5,33 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
+    private static Connection connection = null;
+    private static final String URL = "jdbc:mysql://localhost:3306/sys";
+    private static String USERNAME = "root";
+    private static final String PASSWORD = "test";
+
+
+    private Util (){}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                System.out.println("Соединение с базой данных установлено");
+
+            } catch (SQLException e) {
+                throw new SQLException("Драйвер базы данных не найден", e);
+            }
+        }
+        return connection;
+    }
+
     private static SessionFactory sessionFactory; // КЛАСС КОТОРЫЙ СОЗДАЕТ СОЕДИНЕНИЕ "ФАБРИКА СОЕДИНЕНИЯ"
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
@@ -41,6 +65,14 @@ public class Util {
     public static void shutdown() {
         if (sessionFactory != null) {
             sessionFactory.close();
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e){
+            System.out.println(e);
         }
     }
 
